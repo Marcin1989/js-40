03607fa5fe5a1191d87d9ec3edfb9ac1c75366ba
marcin1989 <marcinlubanski23@gmail.com>
2022-02-08 09:22:10 +0100

    problem-z-js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+function check(n) {
+    if (n >= 100) {
+        console.log("Liczba spelnia warunek");
+    } else if (n <= 200) {
+        console.log("Liczba spelnia warunek");
+    } else if (n > 200) {
+        console.log("Liczba nie spelnia warunku");
+    } else if (n < 100) {
+        console.log("Liczba nie spelnia warunku");
+    }
+
+}
+
+    check(203);
+
+
